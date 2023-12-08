ebe51936a5c9240eb79f0552654c55b6aaa9bc1f
zebra314 <user.name>
2023-12-09 06:46:27 +0800

    Remove logger

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
-import logging
-import logging.handlers
 import os
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from ig import IG
 
-# Set up logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-  "status.log",
-  maxBytes=1024 * 1024,
-  backupCount=1,
-  encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
 # Get the token from github action secret
 try:
   token_ur_always_my = os.environ['TOKEN_UR_ALWAYS_MY']
   token_optimum_pride = os.environ['TOKEN_OPTIMUM_PRIDE']
-  logger.info("Token available.")
+  print("Token available.")
 except:
-  logger.info("Token not available, maybe it is expired.")
+  print("Token not available, maybe it is expired.")
   raise
 
 version = 'v18.0'
-
-logger.info("Start running.")
 ig_ur = IG('ur_always_my', token_ur_always_my, version)
 ig_op = IG('optimum_pride', token_optimum_pride, version)
 
